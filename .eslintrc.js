edc4b635b543d04ce55800c1ad0e70c194acd452
Josh Justice <me@codingitwrong.com>
2022-11-05 07:39:31 -0400

    Webpack setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   env: {
+    browser: true,
     es6: true,
     node: true,
   },
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import './style.css';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
